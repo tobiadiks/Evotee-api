c1fe2f8c1064ebb544cab378526b6ff09d74a239
Qamar Ashfaq <qamarashfaq41@gmail.com>
2021-01-04 08:30:06 +0500

    admin-updated
    work on admin.py to make it easy to use and reliable from admin side.

--- School/admin.py
+++ School/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Electorate
 
+class ElectorateAdmin(admin.ModelAdmin):
+    
+    list_display = ('electorateName','electorateEmail','electorateId','electorateApproved')
+    list_display_links = ('electorateName','electorateEmail')
+    list_editable = ('electorateApproved',)
+    search_fields = ('electorateName','electorateEmail','electorateId')
+    list_per_page = 10
 
-admin.site.register(Electorate)
+
+
+
+
+admin.site.register(Electorate , ElectorateAdmin)
 # Register your models here.
