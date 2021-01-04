f4b6cef4603a1fed2ee8e69a8696e7d68727f015
Qamar Ashfaq <qamarashfaq41@gmail.com>
2021-01-04 19:20:08 +0500

    Serializers CRUD Done

--- Election/urls.py
+++ Election/urls.py
@@ -1,10 +1,36 @@
 from django.urls import path
-from .views import ElectionApi,ElectorateApi, ContestantApi, VotersApi, listElectionApi
-
+from .views import ElectionApi,ElectorateApi,\
+    ContestantApi, VotersApi, listElectionApi,listVoterApi,listContestentApi
+from .import views
 urlpatterns = [
     path('api/create/election', ElectionApi.as_view()),
+
+    #Electorate  urls
     path('api/create/electorate',ElectorateApi.as_view()),
-    path('api/creatcreate/contestant', ContestantApi.as_view()),
+
+    path('api/electorate-detail/<str:pk>/', views.electorateDetail, name="electorate-detail"), # try http://localhost:8000/api/electorate-detail/988063249/
+    path('api/electorate-update/<str:pk>/', views.electorateUpdate, name="electorate-update"), #try http://localhost:8000/api/electorate-update/988063249/
+    path('api/delete-electorate/<str:pk>/', views.electorateDelete, name="delete-electorate"), # http://localhost:8000/api/delete-electorate/988063249/
+
+
+    #contestent CRUD urls
+    path('api/create/contestant', ContestantApi.as_view()),
+    path('api/list/contestent', listContestentApi.as_view()),
+    path('api/contestent-detail/<str:pk>/', views.contestentDetail, name="contestent-detail"),  # try http://localhost:8000/api/contestent-detail/Aqbar Jamiu/
+    path('api/contestent-update/<str:pk>/', views.contestentUpdate, name="contestent-update"),
+    path('api/delete-contestent/<str:pk>/', views.ContestentDelete, name="delete-contestent"),
+
+
+    #Voter CRUD urls
     path('api/create/voters', VotersApi.as_view()),
-    path('api/list/election', listElectionApi.as_view())
+    path('api/list/voters', listVoterApi.as_view()),
+    path('api/voter-detail/<str:pk>/', views.voterDetail, name="task-detail"),
+    path('api/voter-update/<str:pk>/', views.VoterUpdate, name="voter-update"),
+    path('api/delete-voter/<str:pk>/', views.VoterDelete, name="delete-voter"),
+
+    #Election CRUD urls
+    path('api/list/election', listElectionApi.as_view()),
+    path('api/voter-detail/<str:pk>/', views.taskDetail, name="task-detail"),
+    path('api/task-update/<str:pk>/', views.ElectioUpdate, name="election-update"),
+    path('api/delete-election/<str:pk>/', views.ElectionDelete, name="election-delete"),
 ]
--- Election/views.py
+++ Election/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, ListAPIView
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .serializer import ElectionSerializer, ElectorateSerializer, ContestantSerializer,VoterSerializer
 from .models import Election, Contestant
@@ -17,19 +19,150 @@ class ElectorateApi(ListCreateAPIView):
     serializer_class = ElectorateSerializer
     permission_classes = [permissions.IsAdminUser]
 
+# getting details of each electorates on the bases electorateId
+
+@api_view(['GET'])
+def electorateDetail(request, pk):
+	tasks = Electorate.objects.get(electorateId=pk)
+	serializer = ElectorateSerializer(tasks, many=False)
+	return Response(serializer.data)
+
+#  update of each electorates on the bases electorateId
+
+@api_view(['POST'])
+def electorateUpdate(request, pk):
+	task = Electorate.objects.get(electorateId=pk)
+	serializer = ElectorateSerializer(instance=task, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+#  delete electorates on the bases electorateId
+@api_view(['DELETE'])
+def electorateDelete(request, pk):
+    task = Electorate.objects.get(electorateId=pk)
+
+    task.delete()
+
+    return Response('Contestent succsesfully delete!')
+
+
+
+
+
 class ContestantApi(ListCreateAPIView):
     queryset = Contestant.objects.all()
     serializer_class = ContestantSerializer
     permission_classes = [permissions.IsAdminUser]
 
+
+class listContestentApi(ListAPIView):
+    queryset = Contestant.objects.all()
+    serializer_class = ContestantSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+@api_view(['GET'])
+def contestentDetail(request, pk):
+	tasks = Contestant.objects.get(contestantName=pk)
+	serializer = ContestantSerializer(tasks, many=False)
+	return Response(serializer.data)
+
+
+@api_view(['POST'])
+def contestentUpdate(request, pk):
+	task = Contestant.objects.get(contestantName=pk)
+	serializer = ContestantSerializer(instance=task, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def ContestentDelete(request, pk):
+    task = Contestant.objects.get(idNumber=pk)
+
+    task.delete()
+
+    return Response('Contestent succsesfully delete!')
+
+
+
 class VotersApi(ListCreateAPIView):
     queryset = Voter.objects.all()
     serializer_class = VoterSerializer
     permission_classes = [permissions.IsAdminUser]
+
+class listVoterApi(ListAPIView):
+    queryset = Voter.objects.all()
+    serializer_class = VoterSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+
+
+@api_view(['GET'])
+def voterDetail(request, pk):
+	tasks = Voter.objects.get(idNumber=pk)
+	serializer = VoterSerializer(tasks, many=False)
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def VoterUpdate(request, pk):
+	task = Election.objects.get(idNumber=pk)
+	serializer = VoterSerializer(instance=task, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+@api_view(['DELETE'])
+def VoterDelete(request, pk):
+    task = Voter.objects.get(idNumber=pk)
+
+    task.delete()
+
+    return Response('Voter succsesfully delete!')
+
+
     
 class listElectionApi(ListAPIView):
     queryset = Election.objects.all()
     serializer_class = ElectionSerializer
     permission_classes = [permissions.AllowAny]
+
+
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+	tasks = Election.objects.get(electionId=pk)
+	serializer = ElectionSerializer(tasks, many=False)
+	return Response(serializer.data)
+
+
+@api_view(['POST'])
+def ElectioUpdate(request, pk):
+	task = Election.objects.get(electionId=pk)
+	serializer = ElectionSerializer(instance=task, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+
+
+@api_view(['DELETE'])
+def ElectionDelete(request, pk):
+    task = Election.objects.get(electionId=pk)
+
+    task.delete()
+
+    return Response('Election succsesfully delete!')
  
     #TODO:i will be making the update view, it required a lookup field 27/12/2020
